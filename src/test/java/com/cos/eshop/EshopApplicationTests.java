832c2c714913df52edcbd86fc5aec8738a03931c
allintonight <allintonight@naver.com>
2020-11-02 09:38:24 +0900

    first commit

---
+++ src/test/java/com/cos/eshop/EshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cos.eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
